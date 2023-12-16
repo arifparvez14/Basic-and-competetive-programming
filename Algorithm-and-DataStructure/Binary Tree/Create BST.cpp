1e46c35932effc712e5e0c65d87908b9d9416d25
Muhammad Ariful Islam <33893280+arifparvez14@users.noreply.github.com>
2023-12-16 18:41:36 +0600

    Update Create BST.cpp

--- Algorithm-and-DataStructure/Binary Tree/Create BST.cpp
+++ Algorithm-and-DataStructure/Binary Tree/Create BST.cpp
@@ -1,11 +1,14 @@
+///Binary tree
+
+
 /*
-          1
-        /   \
-       2     3
-      /  \   / \
-     4    5 NULL NULL
-     /   \
-    NULL NULL
+           4
+        /     \
+       2       7
+      / \     / \
+     1   3   6   9
+     /    \  /    \
+    NULL NULL NULL NULL
     
  */
 
@@ -32,15 +35,39 @@ struct Node* newNode(int data) {
     return (node);
 }
 
-int main() {
-    struct Node* root = newNode(1);
-    root-> left = newNode(2);
-    root->right = newNode(3);
-    
-    root->left->left = newNode(4);
-    root->left->right = newNode(5);
-    return 0;
+void printList(Node* node) {
+    while(node) {
+        cout<< node->data <<endl;
+        node = node->left;
+    }
 }
 
+void printBT(const std::string& prefix, const Node* node, bool isLeft) {
+    if( node != nullptr ) {
+        cout << prefix;
+        cout << (isLeft ? "├──" : "└──" );
+        cout << node->data << endl;
 
+        printBT( prefix + (isLeft ? "│   " : "    "), node->left, true);
+        printBT( prefix + (isLeft ? "│   " : "    "), node->right, false);
+    }
+}
 
+void printBT(const Node* node) {
+    printBT("", node, false);
+}
+
+int main() {
+    struct Node* root = newNode(4);
+    root-> left = newNode(2);
+    root->right = newNode(7);
+    
+    root->left->left = newNode(1);
+    root->left->right = newNode(3);
+    
+    root->right->left = newNode(6);
+    root->right->right = newNode(9);
+    
+    printBT(root);
+    return 0;
+}
